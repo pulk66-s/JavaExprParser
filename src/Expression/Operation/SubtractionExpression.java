af7e3caf24559ae7e633e3c0dc62591c9d089162
pulk66-s <hugo.marrasse@epitech.eu>
2023-11-10 18:26:41 +0000

    fix: simplification fix

--- src/Expression/Operation/SubtractionExpression.java
+++ src/Expression/Operation/SubtractionExpression.java
@@ -1,8 +1,11 @@
 package Expression.Operation;
 
+import java.util.HashMap;
 import java.util.Optional;
 
+import Exception.VariableNotExistError;
 import Expression.ArithmeticExpression;
+import Expression.ExpressionData;
 import Expression.OperationExpression;
 import Expression.Minimal.NumberExpression;
 
@@ -32,5 +35,30 @@ public class SubtractionExpression extends OperationExpression {
         this.left = left;
         this.right = right;
         this.applyFunction = (Double a, Double b) -> a - b;
+    }
+
+    /**
+     * Simplify the expression
+     * @return The simplified expression
+     * @throws VariableNotExistError
+     */
+    public Optional<ExpressionData> simplify() {
+        Optional<ExpressionData> left = this.left.get().simplify();
+        Optional<ExpressionData> right = this.right.get().simplify();
+        
+        if (!left.isPresent() || !right.isPresent()) {
+            return Optional.empty();
+        }
+    
+        Double constantResult = left.get().constant() - right.get().constant();
+        HashMap<String, Double> leftValue = left.get().variables();
+        HashMap<String, Double> rightValue = right.get().variables();
+        HashMap<String, Double> values = new HashMap<String, Double>();
+
+        values.putAll(leftValue);
+        for (String key : rightValue.keySet()) {
+            values.merge(key, -rightValue.get(key), (a, b) -> a - b);
+        }
+        return Optional.of(new ExpressionData(constantResult, values));
     }
 }
